b8cebd14b9cf867a4e096a51412fff5b850f659a
Mohit <92150824+MKG26@users.noreply.github.com>
2023-10-28 22:08:04 +0530

    Create .gitignore

---
+++ app/src/main/java/com/example/sparsh/ui/theme/Shape.kt
@@ -0,0 +1,9 @@
+package com.example.sparsh.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+
+val Shapes = Shapes(
+    medium = androidx.compose.foundation.shape.RoundedCornerShape(16.dp)
+)
