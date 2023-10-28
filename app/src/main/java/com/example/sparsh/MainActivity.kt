7a26e904b6a784b19c5883558681a5297625779b
harshitsharma25 <omsharma2508@gmail.com>
2023-10-28 22:14:03 +0530

    Revert "Create .gitignore"
    This reverts commit b8cebd14b9cf867a4e096a51412fff5b850f659a.

--- app/src/main/java/com/example/sparsh/MainActivity.kt
+++ app/src/main/java/com/example/sparsh/MainActivity.kt
@@ -1,45 +1,16 @@
 package com.example.sparsh
 
-import android.hardware.ConsumerIrManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.compose.SparshTheme
+import com.example.sparsh.ui.theme.SparshTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,137 +18,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             SparshTheme {
                 // A surface container using the 'background' color from the theme
-
-                    frontApp()
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Harshit")
+                }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SparshTopAppBar(modifier : Modifier = Modifier){
-
-    CenterAlignedTopAppBar(
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
         modifier = modifier
-            .padding(top = 20.dp),
-        title = {
-            Box(
-                modifier = modifier
-                    .padding(top = 10.dp)
-
-
-            ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.logo),
-                        contentDescription = null,
-                        modifier = Modifier
-
-                    )
-            }
-
-
-
-
-        }
     )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun frontApp(
-    modifier: Modifier = Modifier
-) {
-
-
-    Scaffold(
-        topBar = { SparshTopAppBar()},
-        content = {
-
-        Column(
-
-            modifier = modifier
-                .padding(it)
-
-
-        ) {
-            Text(
-                text = "Login As:",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = modifier
-            )
-
-            layout(
-                image = R.drawable.railway,
-                who = R.string.railway,
-
-                )
-
-            layout(
-                image = R.drawable.coal,
-                who = R.string.coal,
-
-                )
-        }
-    })
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun layout(
-    modifier: Modifier = Modifier,
-     image: Int,
-    who : Int
-
-){
-
-    Card(
-        modifier = modifier
-            .clip(MaterialTheme.shapes.medium)
-            .padding(top = 20.dp, start = 20.dp, end = 20.dp),
-        onClick = {}
-    ) {
-        Column(
-            modifier = modifier
-                .padding(top=30.dp, start = 30.dp, end = 30.dp)
-        ) {
-
-
-
-            Image(
-                painter = painterResource(id = image),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = modifier
-                    .size(width = 400.dp, height = 250.dp)
-                    .clip(MaterialTheme.shapes.small)
-                    .padding(bottom = 10.dp)
-            )
-
-            Text(
-
-                text = stringResource(id = who),
-                modifier = modifier
-                    .padding(bottom = 10.dp)
-                    .align(alignment = Alignment.CenterHorizontally),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold
-
-
-            )
-
-
-        }
-    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SparshTheme {
+        Greeting("Android")
     }
 }
-
--- app/src/main/java/com/example/sparsh/ui/theme/Shape.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sparsh.ui.theme
-
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Shapes
-
-val Shapes = Shapes(
-    medium = androidx.compose.foundation.shape.RoundedCornerShape(16.dp)
-)
